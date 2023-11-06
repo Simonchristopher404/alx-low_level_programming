2577efcb83e32610ad7b6bcbacdcb85e8c86a429
Simonchristopher404 <simonchristopher404@gmail.com>
2023-11-06 13:56:20 -0800

    alx task2

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
+
 /**
- * main - prints the name of the file
- *
- * Return: Always 0 (Success)
+ * main - Prints the name of the file of the program
+ *        was compiled from, followed by a new line.
+ * Return: Always 0.
  */
-int ain(void)
+int main(void)
 {
 	printf("%s\n", __FILE__);
+
 	return (0);
 }
