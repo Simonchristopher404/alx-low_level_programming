d955a006e89acae37b7aa0cf1d0945e867f42fd4
Simonchristopher404 <simonchristopher404@gmail.com>
2023-11-14 12:26:43 -0800

    alx task 4

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,4 +1,10 @@
-#ifndef _VARIADIC_FUNCTION_
-#define _VARIADIC_FUNCTION__
+#ifndef _VARIADIC_H_
+#define _VARIADIC_H_
 
-#endif
+int _putchar(char c);
+int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+
+#endif /* _VARIADIC_H_ */
