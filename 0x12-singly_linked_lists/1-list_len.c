e617952e65ceb97b303299d6811ec2afe7c56391
Simonchristopher404 <simonchristopher404@gmail.com>
2023-11-16 13:49:41 -0800

    alx

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,24 +1,20 @@
-
-#include <stdlib.h>
-#include <string.h>
-#include <stdio.h>
 #include "lists.h"
 
-
 /**
-* list_len - check the code for ALX School students.
-* @h: name of the list
-* Return: the number of nodes.
-*/
+ * list_len - counts the number of nodes in a linked list
+ * @h: head of the list
+ *
+ * Return: the number of elements
+ */
 size_t list_len(const list_t *h)
 {
-   int count = 0;
+    const list_t *cursor = h;
+    size_t count = 0;
 
-
-   while (h)
-   {
-       count++;
-       h = h->next;
-   }
-   return (count);
+    while (cursor != NULL)
+    {
+        count += 1;
+        cursor = cursor->next;
+    }
+    return (count);
 }
