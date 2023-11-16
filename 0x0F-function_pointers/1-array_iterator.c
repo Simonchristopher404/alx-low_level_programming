2c1c7922cdd5d0f592da1ee0145315abffcc3b87
Simonchristopher404 <simonchristopher404@gmail.com>
2023-11-15 23:15:14 -0800

    alx task F

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,44 +1,22 @@
+#include "function_pointers.h"
 #include <stdio.h>
-#include <stdlib.h>
 
 /**
- * array_iterator - Iterates an array and executes a function given as a parameter
- * @array: The array to iterate
- * @size: The size of the array
- * @action: A pointer to the function to be used on each element of the array
+ * array_iterator - prints each array element on a new line
+ * @array: array
+ * @size: how many elements to print
+ * @action: pointer to print in regular or hex
+ * Return: void
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	unsigned int i;
 
-	if (array != NULL && action != NULL)
+	if (array == NULL || action == NULL)
+		return;
+
+	for (i = 0; i < size; i++)
 	{
-		for (i = 0; i < size; i++)
-		{
-			action(array[i]);
-		}
+		action(array[i]);
 	}
-}
-
-/**
- * print_element - Prints an integer
- * @element: The integer to be printed
- */
-void print_element(int element)
-{
-	printf("%d\n", element);
-}
-
-/**
- * main - Entry point
- * Return: Always 0
- */
-int main(void)
-{
-	int arr[] = {0, 1, 2, 3, 4};
-	size_t size = sizeof(arr) / sizeof(arr[0]);
-
-	array_iterator(arr, size, print_element);
-
-	return 0;
 }
