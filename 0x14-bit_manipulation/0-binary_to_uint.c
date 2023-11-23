9e58b88f1dd89df0b24df550f5373d2ba09d0ad3
Simonchristopher404 <simonchristopher404@gmail.com>
2023-11-23 13:25:47 -0800

    alx task 14

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * binary_to_uint - Converts a binary number to an unsigned int.
+ * @b: Pointer to a string containing a binary number.
+ *
+ * Return: Unsigned int with decimal value of binary number,
+ *         or 0 if error occurs.
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	int index;
+	unsigned int num;
+
+	num = 0;
+	if (!b)
+		return (0);
+
+	for (index = 0; b[index] != '\0'; index++)
+	{
+		if (b[index] != '0' && b[index] != '1')
+			return (0);
+	}
+
+	for (index = 0; b[index] != '\0'; index++)
+	{
+		num <<= 1;
+		if (b[index] == '1')
+			num += 1;
+	}
+	return (num);
+}
